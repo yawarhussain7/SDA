ac7b2fe26d7a42fa34b0c87a102664492e870891
Yawar Hussain <=>
2025-03-25 07:01:33 -0700

    Fixed the error and add methods

--- Lab2/observer/eventsmanagement/SMSSupportListener.java
+++ Lab2/observer/eventsmanagement/SMSSupportListener.java
@@ -1,11 +1,24 @@
 package eventsmanagement;
 
-public class SMSSupportListener {
+public class SMSSupportListener implements EventListener {
     private static int sms_length = 160;
     private String phone_number;
 
     SMSSupportListener(String phone_number) {
         this.phone_number = phone_number;
+    }
+
+    public void sms_send(String message) {
+        System.out.println("Message Sending " + phone_number + " : " + message);
+    }
+
+    @Override
+    public void update(String eventType, String message) {
+        if (message.length() > sms_length) {
+            System.out.println("Warning: SMS length is execding 160 characters please short it ");
+        } else {
+            sms_send(message);
+        }
     }
 
 }
